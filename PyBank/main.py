2f93b41789f2aab4c29f374467b3cbb25b91b1f5
sporretta <sophiaporretta@icloud.com>
2023-07-03 18:47:14 -0400

    July 3 Progress

--- PyBank/main.py
+++ PyBank/main.py
@@ -24,8 +24,8 @@ with open(csvpath) as csvfile:
     csv_header= next(budget_data)
 
 #Print Analysis header
-    print("Financial Analysis")
-    print("---------"*10)    
+   # print("Financial Analysis")
+    #print("---------"*10)    
 
  #Set lists to hold data from csv
     Date=[]
@@ -44,36 +44,63 @@ with open(csvpath) as csvfile:
         TotalDollars+= int(row[1])
    
     
-    print("Total Months: " + str(rowcount))
-    print("Total: $" + str(TotalDollars))
+   # print("Total Months: " + str(rowcount))
+    #print("Total: $" + str(TotalDollars))
     
 #Use the values from the new profit list to find the change from month to month
     Change=[Profit[i+1]- Profit[i] for i in range (len(Profit)-1)]
     
-    Date_Change=[Date[i+1] for i in range(len(Date)-2)]
-    combined=[[Date_Change],[Change]]
- 
+ #Find change in date
+    Date_Change=[Date [i+1] for i in range (len(Date)-1)]
+
+ #Combine the lists
+   # change_data=list(zip(Date_Change,Change))
+    
+   
 #find the average change
     length=0
     for j in Change:
         length= length +1
     Average=round(sum(Change)/len(Change),2)
 
-    print("Average Change: $" + str(Average))
+    #print("Average Change: $" + str(Average))
 
 #Find the greatest increase in the list
-    
+#78th row
+   # MaxInc_Date=Date_Change[78]
     MaxIncrease=max(Change)
-    for change in combined:
-        if change==MaxIncrease:
-            print(Date_Change)
 
+    MaxIndex=Change.index(MaxIncrease)
+    MaxInc_Date=Date_Change[MaxIndex]
    
-#Find the date of the greatest increase
-   
-
-    print(f"The Greatest Increase in Profits: (${ MaxIncrease})")
+    #print(f"The Greatest Increase in Profits: {MaxInc_Date} (${ MaxIncrease})")
 
 #Find the greatest decrease
+#48th row
+    #MaxDec_Date=Date_Change[48]
     MaxDecrease=min(Change)
-    print(f"The Greatest decrease in Profits: (${MaxDecrease})")
+  
+    MinIndex=Change.index(MaxDecrease)
+    MaxDec_Date=Date_Change[MinIndex]
+
+
+    #print(f"The Greatest decrease in Profits: {MaxDec_Date} (${MaxDecrease})")
+
+Output_Text=(
+f'Financial Analysis\n'
+f'----------------------------\n'
+f'Total Months: {rowcount}\n'
+f'Total: ${TotalDollars}\n'
+f'Average Change: ${Average}\n'
+f'Greatest Increase in Profits: {MaxInc_Date} (${ MaxIncrease})\n'
+f'Greatest Decrease in Profits: {MaxDec_Date} (${MaxDecrease})'
+)
+print(Output_Text)
+
+
+#write results to text file
+output_path=os.path.join("Analysis/PyBank_Output.txt")
+
+with open(output_path,"w") as csvfile:
+
+    csvfile.write(Output_Text)
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -14,10 +14,10 @@ import csv
 csvpath = os.path.join("Resources", "election_data.csv")
 
 
-#Assign lists to store the data for each column
-VoterID=[]
-County=[]
+#Assign lists to store the data for eeach candidate
 Candidate=[]
+Candidate_Dictionary={}
+rowcount=0
 
 #Reading using csv
 with open(csvpath) as csvfile:
@@ -27,76 +27,44 @@ with open(csvpath) as csvfile:
 #Read header row
     csv_header= next(election_data)
 
-#Set calculated variables to 0
-    rowcount=0
+
 
 #iterate through rows to append csv data to a list
     for row in election_data:
-        #Add number of votes
-        VoterID.append(row[0])
-        #Add county each vote was from
-        County.append(row[1])
         #Add candidate each vote was for
-        Candidate.append(row[2]) 
         rowcount+=1
+        if row[2] not in Candidate:
+            Candidate.append(row[2]) 
+            Candidate_Dictionary[row[2]]=0
+        Candidate_Dictionary[row[2]]+=1
+    #print(Candidate_Dictionary)
 
-#Print Election Results header
-
-print("Election Results")
-print("------"*10)  
-print("Total Votes"+str(rowcount))
-print("------"*10) 
-
-
-#Create list for unique candidate names
-Names=[]
-
-#Print the Candidate names
-for rep in Candidate:
-    if rep not in Names:
-        Names.append(rep)    
-
-#Count the votes for each name
-Name1=Names[0]
-Name2=Names[1]
-Name3=Names[2]
-Name1_Count=0
-Name2_Count=0
-Name3_Count=0
-
-for name in Candidate:
-    if name==Name1:
-        Name1_Count+=1
-    if name==Name2:
-        Name2_Count+=1
-    if name==Name3:
-        Name3_Count+=1
-
-#find percentage of total votes for each
-
-n1_percent=round(Name1_Count/rowcount * 100,3)
-n2_percent=round(Name2_Count/rowcount * 100,3)
-n3_percent=round(Name3_Count/rowcount * 100,3)
+    Candidate_Percent = {key: round(val / rowcount*100,2) for key, val in Candidate_Dictionary.items()}
+    #print(Candidate_Percent)
+    Winner = max(Candidate_Dictionary, key=Candidate_Dictionary.get)
+    #print(Winner)
 
 
-#print candidate summary
+    TotalVotes= "Total Votes: "+str(rowcount)  
 
-print(Names[0] + ": " + str(n1_percent) + " %" + " ("+ str(Name1_Count) + ")")
-print(Names[1] + ": " + str(n2_percent) + " %" + " ("+ str(Name2_Count) + ")")
-print(Names[2] + ": " + str(n3_percent) + " %" + " ("+ str(Name3_Count) + ")")
-print("------"*10) 
 
-#Find the winner of the election
+Output_Text=(
+f'Election Results\n'
+f'-------------------------\n'
+f'Total Votes: {rowcount}\n'
+f'-------------------------\n'
+for k,v in Candidate_Dictionary
+    f'{k}: {Candidate_Percent}% ({v})\n'
+    
+f'-------------------------\n'
+f'Winner: {Winner}\n'
+f'-------------------------')
 
-if Name1_Count>Name2_Count:
-    if Name1_Count>Name3_Count:
-        print("Winner: " + Names[0])   
-elif Name2_Count>Name3_Count:
-    print("Winner: " + Names[1]) 
-elif Name3_Count>Name2_Count:
-    print("Winner: " + Names[2])    
-print("------"*10)     
-
+print(Output_Text)
 
 #Write output to text file
-cleaned_list=list(zip)
+output_path=os.path.join("Analysis/PyPollOutput.txt")
+
+with open(output_path,"w") as csvfile:
+
+    csvfile.write(Output_Text)
